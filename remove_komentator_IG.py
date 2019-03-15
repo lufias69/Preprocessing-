520a7095e2a13734fb180f9c8678ebfd97235cbc
lufias69 <syaifulbachrimustamin@gmail.com>
2019-03-15 22:34:49 +0700

    Update remove_komentator_IG.py

--- remove_komentator_IG.py
+++ remove_komentator_IG.py
@@ -1,7 +1,8 @@
 def remomve_komentator(komentar):
     komentar2 = []
     for i in komentar:
-        if b[0] == "@":
+        a = i.split()
+        if i[0] == "@":
             komentar2.append(" ".join(a[1:]))
         else:
             komentar2.append(i)
